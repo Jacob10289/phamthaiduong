18b35473f9abf6a66ed3a688ca2d388a868b9d15
phamthaiduong <nguyentiengpg@gmail.com>
2025-04-29 22:55:05 +0700

    Update app.py
    Signed-off-by: phamthaiduong <nguyentiengpg@gmail.com>

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Dữ liệu mẫu - danh sách bài viết
+posts = [
+    {
+        'title': 'Mùa thu của Nguyễn Khuyến',
+        'author': 'admin',
+        'content': 'Bài thơ nổi tiếng với hình ảnh ao thu trong trẻo...',
+    },
+    {
+        'title': 'Chiếc thuyền ngoài xa - Nguyễn Minh Châu',
+        'author': 'user1',
+        'content': 'Tác phẩm phản ánh hiện thực và chiều sâu của nghệ thuật...',
+    }
+]
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(debug=True)
