8d92a709eff5f530e668f983c32ea4d222b43d83
phamthaiduong <nguyentiengpg@gmail.com>
2025-05-01 23:54:09 +0700

    Update app.py
    Signed-off-by: phamthaiduong <nguyentiengpg@gmail.com>

--- app.py
+++ app.py
@@ -1,33 +1,20 @@
-app.py :
-
 from flask import Flask, render_template, request, redirect, url_for
-
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        # xử lý form đăng ký ở đây
+    if request.method == 'POST':   
         return redirect(url_for('index'))
     return render_template('register.html')
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # xử lý đăng nhập ở đây
+    if request.method == 'POST':      
         return redirect(url_for('index'))
     return render_template('login.html')
-
 @app.route('/post', methods=['GET', 'POST'])
 def post():
-    if request.method == 'POST':
-        # xử lý bài viết ở đây
+    if request.method == 'POST':       
         return redirect(url_for('index'))
     return render_template('post.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
